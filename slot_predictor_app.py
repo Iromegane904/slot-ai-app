dd1f3ca710cf25a10d929749d74239d6c4f0d560
Iromegane904 <masaya1929153@icloud.com>
2025-04-19 16:07:09 +0900

    Add files via upload

--- slot_predictor_app.py
+++ slot_predictor_app.py
@@ -20,22 +20,36 @@ with st.form("predict_form"):
     with col1:
         店舗選択 = st.selectbox("店舗", list(店舗マップ.keys()))
         台選択 = st.selectbox("台の種類", list(台マップ.keys()))
-        総回転数 = st.number_input("総回転数", min_value=0)
-        現在G = st.number_input("現在のゲーム数", min_value=0)
-        差枚 = st.number_input("差枚", min_value=-5000, max_value=5000)
+        総回転数 = st.text_input("総回転数（例: 3500）")
+        現在G = st.text_input("現在のゲーム数（例: 200）")
+        差枚 = st.text_input("差枚（例: -500）")
 
     with col2:
-        前回当選G = st.number_input("前回の当選ゲーム数", min_value=0)
-        平均当選G = st.number_input("平均当選ゲーム数", min_value=0)
-        連チャン = st.number_input("3連以上の連チャン回数", min_value=0)
-        駆け抜け = st.number_input("駆け抜け回数", min_value=0)
-        当たり回数 = st.number_input("当たり回数", min_value=0)
+        前回当選G = st.text_input("前回の当選ゲーム数")
+        平均当選G = st.text_input("平均当選ゲーム数")
+        連チャン = st.text_input("3連以上の連チャン回数")
+        駆け抜け = st.text_input("駆け抜け回数")
+        当たり回数 = st.text_input("当たり回数")
 
     submitted = st.form_submit_button("🔮 予測する")
 
 if submitted:
+    def safe_int(value):
+        try:
+            return int(value)
+        except:
+            return 0
+
     店舗 = 店舗マップ[店舗選択]
     台の種類 = 台マップ[台選択]
+    総回転数 = safe_int(総回転数)
+    現在G = safe_int(現在G)
+    前回当選G = safe_int(前回当選G)
+    平均当選G = safe_int(平均当選G)
+    差枚 = safe_int(差枚)
+    連チャン = safe_int(連チャン)
+    駆け抜け = safe_int(駆け抜け)
+    当たり回数 = safe_int(当たり回数)
 
     input_data = [[店舗, 台の種類, 総回転数, 現在G, 前回当選G,
                    平均当選G, 差枚, 連チャン, 駆け抜け, 当たり回数]]
